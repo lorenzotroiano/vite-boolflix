804fc68f3b1532c37660d53f159d73cbf16e9519
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-06-08 17:41:28 +0200

    aggiunto logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,6 +10,10 @@ export default {
 
 <template>
     <header>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Netflix_logo_%282%29.svg/1200px-Netflix_logo_%282%29.svg.png"
+                alt="">
+        </div>
         <h1>BoolFlix</h1>
 
 
@@ -18,7 +22,16 @@ export default {
 
 <style lang="scss" scoped>
 header {
+    display: flex;
+    align-items: center;
+
     padding: 10px;
     background-color: violet;
+
+
+    img {
+        width: 150px;
+        margin-right: 30px;
+    }
 }
 </style>
